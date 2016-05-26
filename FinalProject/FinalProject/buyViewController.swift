33221bfc1877c81323861ce0a92c8951a938f040
Totoro338 <dennisplun@gmail.com>
2016-05-26 15:59:21 +0800

    20160526

--- FinalProject/FinalProject/buyViewController.swift
+++ FinalProject/FinalProject/buyViewController.swift
@@ -16,49 +16,82 @@ class buyViewController: UIViewController, UITableViewDataSource {
     var ref = Firebase(url: "https://sizzling-heat-4632.firebaseio.com")
     
     var uid = ""
-    
+    var selectedRow:Int = -1
+
+   
+   
     @IBOutlet weak var tableView: UITableView!
     
-    var productDescription:[String] = ["Des1", "Des2"]
-    var productPrice:[Int] = [1, 2]
-    var productDate:[String] = ["Date1", "Date2"]
-    var productImage:[String] = ["Image1", "Image2"]
+    var productDescription:[String] = []
+    var productPrice:[Int] = []
+    var productDate:[String] = []
+    var productImage:[String] = []
+    var productAutoID:[String] = []
     
+    @IBAction func buyItem(sender: AnyObject) {
+        print(selectedRow)
+        if selectedRow != -1 {
+        ref.childByAppendingPath("Sold").setValue(ref.childByAppendingPath(productAutoID[selectedRow]))
+        }
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableView.dataSource = self
         
-        
-//        if ref.authData != nil {
-//            uid = ref.authData.uid
-//            
-//            ref.childByAppendingPath("ForSale")
-//                .observeEventType(.ChildAdded, withBlock: {
-//                    snapshot in
-//                    
-//                    if let content = snapshot.value["Description"] as? String {
-//                        self.productDescription.append(content)
-//                        
-//                        self.tableView.reloadData()
-//                    }
-//                    
-//                    
-//                })
-//            
-//            
-//            
-//        } else {
-//            print("not logged in!!")
-//        }
+        if ref.authData != nil {
+            uid = ref.authData.uid
+            
+            ref.childByAppendingPath("ForSale")
+                .observeEventType(.ChildAdded, withBlock: {
+                    snapshot in
+                    
+                    if let content = snapshot.value["Description"] as? String {
+                        self.productDescription.append(content)
+                    }
+                    
+                    if let content = snapshot.value["Price"] as? Int {
+                        self.productPrice.append(content)
+                    }
+                    
+                    if let content = snapshot.value["Date"] as? String {
+                        self.productDate.append(content)
+                    }
+                    
+                    if let content = snapshot.value["Image"] as? String {
+                        self.productImage.append(content)
+                    }
+
+                    if let content = snapshot.key as? String {
+                        self.productAutoID.append(content)
+                    }
+
+                    self.tableView.reloadData()
+                    
+                })
+            
+            
+            
+        } else {
+            print("not logged in!!")
+        }
         
         
     }
 
+    func tableView
    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        selectedRow = indexPath.row
+    }
+    
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return productDescription.count;
     }
+
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
@@ -67,16 +100,14 @@ class buyViewController: UIViewController, UITableViewDataSource {
         
         if let cell = cell as? ProductTableViewCell {
             cell.labelDescription.text = productDescription[indexPath.row]
-            cell.labelPrice.text = String(productPrice[indexPath.row])
-            cell.labelDate.text = productDate[indexPath.row]
-            
+            cell.labelPrice.text = "$ " + String(productPrice[indexPath.row])
+            cell.labelDate.text = "Arrive on " + productDate[indexPath.row]
+            let imageData = NSData(base64EncodedString: productImage[indexPath.row] as! String,
+                                   options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
+            let decodedImage = UIImage(data:imageData!)
+            cell.productImage.image = decodedImage
 
         }
-        
-        return cell
-
-        // modify the cell
-        
         
         return cell
         
